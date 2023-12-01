1ba4e4c60b3f806631e87fe074c7207ab2617e79
Cubit-Bites <152619896+satrio12312@users.noreply.github.com>
2023-12-02 05:45:16 +0700

    Delete Cubit-Bites directory

--- Cubit-Bites/js/script.js
+++
@@ -1,6 +0,0 @@
-// Toggle class active
-const NavbarNav = document.querySelector(".navbar-nav");
-// ketika humberger menu di klik
-document.querySelector("#hamburger-menu").onclick = () => {
-  NavbarNav.classList.toggle("active");
-};
